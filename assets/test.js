1c3ffbfd5c75879a1203cd88d7f601fdd884f138
fbenpao <1250222077@qq.com>
2020-12-29 10:48:09 +0800

    add js call dart

---
+++ assets/test.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<button onclick="callFlutter()">callFlutter</button>
+
+<script>
+  function callFlutter(){
+  Toast.postMessage("JS调用了Flutter");
+  }
+  </script>
+
+</body>
+</html>
